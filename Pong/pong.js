362eb8e0044759b1d125116f93328de75af6ff98
pkowalski3110 <piotr.kowalski3110@gmail.com>
2022-05-23 14:18:07 +0200

    pong js

--- Pong/pong.js
+++ Pong/pong.js
@@ -16,10 +16,10 @@ class Background {
         ctx.lineWidth = 4;
 
         ctx.beginPath();
-        ctx.moveTo(10, 40);
-        ctx.lineTo(this.width - 10, 40);
-        ctx.moveTo(10, this.height - 10);
-        ctx.lineTo(this.width - 10, this.height - 10);
+        ctx.moveTo(0, 40);
+        ctx.lineTo(this.width - 0, 40);
+        ctx.moveTo(0, this.height - 10);
+        ctx.lineTo(this.width - 0, this.height - 10);
         ctx.stroke();
 
         ctx.beginPath();
@@ -34,21 +34,37 @@ class Background {
     }
 }
 
+class Player {
+    constructor(x){
+        this.canvas = document.createElement("CANVAS");
+        this.canvas.setAttribute("width", 8);
+        this.canvas.setAttribute("height", 60);
+        this.x = x;
+        const ctx = this.canvas.getContext('2d');
+        ctx.fillStyle = "white";
+        ctx.fillRect(0,0,8,60);
+    }
+    draw(ctx){
+        ctx.drawImage(this.canvas, this.x, this.y);
+    }
+}
+
 class Pong {
     constructor(width, height) {
         this.background = new Background(width, height);
+        this.player = new Player(15);
         this.canvas = document.createElement('CANVAS');
         this.canvas.setAttribute("width", width);
         this.canvas.setAttribute("height", height);
         this.ctx = this.canvas.getContext('2d');
         this.width = width;
         this.height = height;
-        this.Player = new Player(15);
-
-        this.canvas.addEventListener('mousemove', e => {
-            this.player.y = e.offsetY;
-            this.draw();
+        this.player
+        this.canvas.addEventListener('mousemove', e=>{
+        this.player.y = e.offsetY;
+        this.draw();
         });
+
     }
 
     init() {
@@ -58,29 +74,10 @@ class Pong {
 
     draw() {
         this.background.draw(this.ctx);
-        this.Player.draw(this.ctx, 100);
-        window.requestAnimationFrame(() => this.draw());
+        this.player.draw(this.ctx, 100);
+        window.requestAnimationFrame(()=>this.draw());
     }
 }
 
-class Player {
-    constructor(x) {
-        this.canvas = document.createElement('CANVAS');
-        this.canvas.setAttribute("width", 8);
-        this.canvas.setAttribute("height", 60);
-        this.x = x;
-
-        const ctx = this.canvas.getContext('2d');
-        ctx.fillStyle = "white";
-        ctx.fillRect(0, 0, 8, 60);
-    }
-
-    draw(ctx, y) {
-        ctx.drawImage(this.canvas, this.x, y)
-    }
-
-
-}
-
-const game = new Pong(800, 600);
+const game = new Pong(1000, 600);
 window.addEventListener('DOMContentLoaded', () => game.init());

ea88f41e6edafd0009182c8471b7c513be5427e7
pkowalski3110 <piotr.kowalski3110@gmail.com>
2022-05-25 17:23:30 +0200

    pong nie dziala

--- Pong/pong.js
+++ Pong/pong.js
@@ -9,6 +9,60 @@ const FIELD_LINE_WIDTH = 4;
 const MOVE_MIN = MARGIN_TOP + FIELD_LINE_WIDTH * 0.5 + 1;
 const MOVE_MAX = GAME_HEIGHT - PLAYER_HEIGHT - MARGIN_BOTTOM - FIELD_LINE_WIDTH * 0.5 - 1;
 const PLAYER_INITIAL_Y = Math.round((GAME_HEIGHT - MARGIN_TOP - MARGIN_BOTTOM - PLAYER_HEIGHT) * 0.5 + MARGIN_TOP);
+const CENTER_X = GAME_WIDTH * 0.5;
+const CENTER_Y = Math.round((GAME_HEIGHT + MARGIN_TOP - MARGIN_BOTTOM) * 0.5);
+const BALL_SIZE = 12;
+
+class Ball {
+    constructor() {
+        this.canvas = document.createElement('CANVAS');
+        this.canvas.setAttribute("width", BALL_SIZE);
+        this.canvas.setAttribute("height", BALL_SIZE);
+        this.x = CENTER_X - BALL_SIZE * 0.5;
+        this.y = CENTER_Y - BALL_SIZE * 0.5;
+        this.m = {x: -1, y: -1};
+        this.speed = 2;
+        this.over = false;
+
+        const ctx = this.canvas.getContext('2d');
+        ctx.fillStyle = "white";
+        ctx.fillRect(0, 0, BALL_SIZE, BALL_SIZE);
+    }
+
+    draw(ctx) {
+        ctx.drawImage(this.canvas, this.x, this.y)
+    }
+
+    move(leftPlayerY, rightPlayerY) {
+        if (this.y <= MOVE_MIN && this.m.y < 0) {
+            this.m.y = 1;
+        }
+        if (this.y >= (MOVE_MAX + PLAYER_HEIGHT - BALL_SIZE) && this.m.y > 0) {
+            this.m.y = -1;
+        }
+        if(!this.over && this.x <= (15+PLAYER_WIDTH) && this.m.x < 0){
+            if((this.y + BALL_SIZE) >= leftPlayerY && this.y <= (leftPlayerY + PLAYER_HEIGHT)){
+                this.m.x=1;
+            }
+            else {
+                this.over = true;
+            }
+        }
+        if (this.x <= 0 && this.m.x < 0) {
+            this.m.x = 1;
+        }
+        if (this.x >= (GAME_WIDTH - BALL_SIZE) && this.m.x > 0) {
+            this.m.x = -1;
+        }
+        if(this.x <= 15 + PLAYER_WIDTH && this.m.x < 0)
+        {
+            this.m.x = 1;
+        }
+
+        this.x += this.m.x * this.speed;
+        this.y += this.m.y * this.speed;
+    }
+}
 
 class Background {
     constructor(width, height) {
@@ -49,6 +103,7 @@ class Background {
 class Pong {
     constructor(width, height) {
         this.background = new Background(width, height);
+        this.ball = new Ball();
         this.canvas = document.createElement('CANVAS');
         this.canvas.setAttribute("width", width);
         this.canvas.setAttribute("height", height);
@@ -77,19 +132,28 @@ class Pong {
         this.background.draw(this.ctx);
         this.playerA.draw(this.ctx, 100);
         this.playerB.draw(this.ctx, 100);
-        window.requestAnimationFrame(()=>this.draw());
+        this.ball.draw(this.ctx);
+        this.ball.move(this.playerA.y);
+        window.requestAnimationFrame(() => this.draw());
     }
 
-    handlekey(state, event){
-        console.log(`${event.code} is ${state} (${event.repeat})`)
-        if(state === 'down' && event.key === 'ArrowUp'){
-            this.playerA.moveUp();
-        }
-        else if (state === 'down' && event.key === 'ArrowDown'){
-            this.playerA.moveDown();
-        }
-        else{
-            this.playerA.stop();
+    handlekey(state, event) {
+        console.log(`${event.code} is ${state} (${event.repeat})`);
+        const keyPressed = state === 'down';
+
+        switch (event.code) {
+            case 'KeyW':
+                keyPressed ? this.playerA.moveUp() : this.playerA.stop();
+                break;
+            case 'KeyS':
+                keyPressed ? this.playerA.moveDown() : this.playerA.stop();
+                break;
+            case 'ArrowUp':
+                keyPressed ? this.playerB.moveUp() : this.playerB.stop();
+                break;
+            case 'ArrowDown':
+                keyPressed ? this.playerB.moveDown() : this.playerB.stop();
+                break;
         }
     }
 }
@@ -114,36 +178,37 @@ class Player {
         this.parentCTX.drawImage(this.canvas, this.x, this.y)
     }
 
-    move(){
+    move() {
         this.y += this.speed;
 
-        if (this.y > MOVE_MAX){
+        if (this.y > MOVE_MAX) {
             this.y = MOVE_MAX;
             this.stop();
-        }
-        else if (this.y < MOVE_MIN){
+        } else if (this.y < MOVE_MIN) {
             this.y = MOVE_MIN;
             this.stop();
         }
-        if (this.isMoving){
+        if (this.isMoving) {
             setTimeout(() => this.move(), 20);
         }
     }
 
-    stop(){
+    stop() {
         this.speed = 0;
         this.isMoving = false;
 
     }
 
-    moveDown(){
-        this.speed = 2;
+    moveDown() {
+        if (this.isMoving) return;
+        this.speed = 8;
         this.isMoving = true;
         this.move();
     }
 
-    moveUp(){
-        this.speed = -2;
+    moveUp() {
+        if (this.isMoving) return;
+        this.speed = -8;
         this.isMoving = true;
         this.move();
     }
